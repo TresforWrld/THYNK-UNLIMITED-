4e666ce0c40ae7bf533252e6bb901ab266c86c19
TresforWrld <tresforzulu@gmail.com>
2026-03-21 16:17:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,28 @@
-const CACHE = 'anicade-v5';
+var CACHE_NAME = 'anicade-tech-v1';
+var urlsToCache = [
+  '/ANICADEtech.net-in/',
+  '/ANICADEtech.net-in/index.html',
+  '/ANICADEtech.net-in/manifest.webmanifest',
+  '/ANICADEtech.net-in/icon-192.png',
+  '/ANICADEtech.net-in/icon-512.png'
+];
 
 self.addEventListener('install', function(e) {
   self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE).then(function(c) {
-      return c.addAll(['./index.html', './manifest.json', './icon-192.png', './icon-512.png']).catch(function(){});
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache).catch(function(){});
     })
   );
 });
 
 self.addEventListener('activate', function(e) {
   e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n){ return n !== CACHE_NAME; })
+             .map(function(n){ return caches.delete(n); })
+      );
     }).then(function(){ return self.clients.claim(); })
   );
 });
@@ -21,11 +31,12 @@ self.addEventListener('fetch', function(e) {
   if (e.request.method !== 'GET') return;
   if (e.request.url.indexOf('jsonbin.io') > -1) return;
   if (e.request.url.indexOf('fonts.googleapis') > -1) return;
+  if (e.request.url.indexOf('imgur.com') > -1) return;
   e.respondWith(
     fetch(e.request).then(function(r) {
-      if (r && r.status === 200) {
+      if (r && r.status === 200 && r.type !== 'opaque') {
         var clone = r.clone();
-        caches.open(CACHE).then(function(c){ c.put(e.request, clone); });
+        caches.open(CACHE_NAME).then(function(c){ c.put(e.request, clone); });
       }
       return r;
     }).catch(function() {
